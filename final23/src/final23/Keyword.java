c5788f2bd6c72fe05891df1aabdf1c5ddbd35914
jasonyang <jasonyang@10.232.75.32>
2022-11-09 17:45:54 +0800

    Pushing new classes.

---
+++ final23/src/final23/Keyword.java
@@ -0,0 +1,16 @@
+package final23;
+
+public class Keyword {
+	public String name;
+	public double weight;
+	
+	public Keyword(String name, double weight){
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	@Override
+	public String toString(){
+		return "["+name+","+weight+"]";
+	}
+}
