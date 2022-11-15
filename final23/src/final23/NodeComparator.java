053e1cbe0e8464b91d536ac12a16fe56c1bcb742
jasonyang <jasonyang@localhost>
2022-11-15 14:44:05 +0800

    Change and add classes.(11/15)

---
+++ final23/src/final23/NodeComparator.java
@@ -0,0 +1,17 @@
+package final23;
+import java.util.Comparator;
+
+public class NodeComparator implements Comparator<WebNode>{
+
+	@Override
+	public int compare(WebNode o1, WebNode o2) {
+		// TODO Auto-generated method stub
+		if(o1.nodeScore > o2.nodeScore){
+			return 1;
+		}else if(o1.nodeScore < o2.nodeScore) {
+			return -1;
+		}
+		return 0;
+	}
+	
+}
--- final23/src/final23/Ranking.java
+++ final23/src/final23/Ranking.java
@@ -1,5 +1,21 @@
 package final23;
+import java.util.PriorityQueue;
 
 public class Ranking {
+	PriorityQueue<WebNode> result;
+	
+	public Ranking(){
+		this.result = new PriorityQueue<WebNode>(100, new NodeComparator());
+	}
+	
+	public void output(){
+		//3. print the output in order and remove all element
+		StringBuilder sb = new StringBuilder();
+		while(!(result.isEmpty())) {
+			sb.append(result.remove().toString());
+		}
+		
+		System.out.println(sb.toString());	
+	}
 
 }
