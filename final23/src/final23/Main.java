6b0bd953e08f10d50604fe2904486ea0c772fb41
dnny60 <dnny60@gmail.com>
2022-11-28 17:22:19 +0800

    17:22 Danny solve url problems

--- final23/src/final23/Main.java
+++ final23/src/final23/Main.java
@@ -6,17 +6,12 @@ import java.net.URLDecoder;
 public class Main {
 	
 	public static void main(String args[]) throws IOException {
-<<<<<<< HEAD
 		Search go = new Search("Justin");
 		System.out.println("hello");
-=======
-		Search go = new Search("臺灣");
->>>>>>> branch 'master' of https://github.com/winnie2qui0/team-23
 		HashMap<String, String> urls = go.query();
 		System.out.println(urls);
 		Ranking rank = new Ranking();
 		for(String title : urls.keySet()) {
-<<<<<<< HEAD
 			System.out.println(title);
 			String encodedURL = urls.get(title);
 			System.out.println("  encoded url ");
@@ -36,11 +31,6 @@ public class Main {
 			    	System.out.println("url decode problem");
 			    }
 			BuildTree urlTree = new BuildTree(decodedURL, title);
-=======
-//			System.out.print(title);
-//			System.out.println(urls.get(title));
-			BuildTree urlTree = new BuildTree(urls.get(title), title);
->>>>>>> branch 'master' of https://github.com/winnie2qui0/team-23
 			WebTree tree = urlTree.buildIt();
 			tree.setPostOrderScore();
 			rank.add(tree.root);
@@ -55,10 +45,6 @@ public class Main {
 //		tree = urlTree.buildIt();
 //		tree.setPostOrderScore();
 //		rank.add(tree.root);
-<<<<<<< HEAD
 //		rank.output();
-=======
-		rank.output();
->>>>>>> branch 'master' of https://github.com/winnie2qui0/team-23
 	}
 }
