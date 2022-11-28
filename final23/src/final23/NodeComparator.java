99f5afb35ac9980c3e4652f979bce4ee4fe4b321
jasonyang <jasonyang@10.232.203.251>
2022-11-28 12:22:20 +0800

    MinHeap -> MaxHeap

--- final23/src/final23/NodeComparator.java
+++ final23/src/final23/NodeComparator.java
@@ -5,9 +5,9 @@ public class NodeComparator implements Comparator<WebNode> {
 	@Override
 	public int compare(WebNode o1, WebNode o2) {
 		// TODO Auto-generated method stub
-		if (o1.nodeScore > o2.nodeScore) {
+		if (o1.nodeScore < o2.nodeScore) {
 			return 1;
-		} else if (o1.nodeScore < o2.nodeScore) {
+		} else if (o1.nodeScore > o2.nodeScore) {
 			return -1;
 		}
 		return 0;
