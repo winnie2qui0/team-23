c5abf1560bdd4a147434fdc71a5d9b0479223a58
user <user@10.232.194.166>
2023-01-03 14:22:26 +0800

    Danny 1/3 重析出發點!!!!!!!

--- final23/src/final23/controller/UserController.java
+++ final23/src/final23/controller/UserController.java
@@ -2,14 +2,16 @@ package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 
 public class UserController {
 	
-	@GetMapping(value = "/api/myuser")
-	public String user() {
-		return "Welcome";
-	}
+	 @GetMapping("/hello")
+	    public String hello() {
+	        return "hello"; // 要導入的html
+	    } 
 
 }
