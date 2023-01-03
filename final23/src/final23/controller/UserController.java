4ea896076c0b5b296359d3aa9ae41d40022b6883
user <user@10.232.194.166>
2023-01-03 16:23:11 +0800

    從網站輸入的東西，在後端可以收到了!

--- final23/src/final23/controller/UserController.java
+++ final23/src/final23/controller/UserController.java
@@ -1,17 +1,47 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class UserController {
 	
+	 @GetMapping("/greeting")
+	    public String hello(Model model) {
+		 
+		 model.addAttribute("greeting", new Greeting());
+	        return "greeting"; 
+	    }
+	 
+	 @PostMapping("/greeting")
+	  public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+	    model.addAttribute("greeting", greeting);
+	    return "result";
+	  }
+	 
 	 @GetMapping("/index")
-	    public String hello() {
-	        return "index"; // 要導入的html
-	    } 
+	    public String getIndex(@ModelAttribute UserForm userForm, Model model) {
+		 
+		 model.addAttribute("userForm", new UserForm());
+		 //System.out.println(userForm.getContent());
+	        return "index"; 
+	    }
+	 
+	 @PostMapping("/index")
+	  public String indexForm(@ModelAttribute UserForm userForm, Model model) {
+	    model.addAttribute("userForm", userForm);
+	    System.out.println(userForm.getContent());
+	    return "index";
+	  }
+	 
+	 
 
 }
---
+++ final23/src/final23/controller/UserForm.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class UserForm {
+	
+	private String content;
+	
+	public String getContent() {
+	    return content;
+	  }
+
+	  public void setContent(String content) {
+	    this.content = content;
+	  }
+
+
+}
